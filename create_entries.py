029d6512caa93c3bc9cf2349ddec1eb9cc12434f
Lorenz Schmid <lorenzschmid@users.noreply.github.com>
2018-12-11 11:04:14 +0100

    made filename generation safer

--- create_entries.py
+++ create_entries.py
@@ -2,13 +2,23 @@
 
 import os
 import csv
-import re
 import yaml
+import re
+from unicodedata import normalize
 
 
 def transform_title(string):
+    # replace spaces
     new_string = string.replace(' ', '_')
+
+    # replace special characters (:, /, ...)
     new_string = re.sub(r'(?u)[^-\w.]', '', new_string)
+
+    # remove URL unsafe characters (ä, ö, ü, é, è, à, ...)
+    new_string = normalize(
+        'NFKD', new_string).encode('ASCII', 'ignore').decode("utf-8")
+
+    # work only in lower case
     new_string = new_string.lower()
 
     return new_string
